a68c9c4f1507857a8a48b723b366fd8121d85919
ktruedat <crinnic77@gmail.com>
2024-01-28 19:49:38 +0200

    adding client side validation

--- shopChart/Models/ProductModel.cs
+++ shopChart/Models/ProductModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using shopChart.Data;
 
 namespace shopChart.Models;
@@ -5,8 +7,14 @@ namespace shopChart.Models;
 public class ProductModel
 {
     public int Id { get; set; }
+    [Required]
+    [DisplayName("Product Name")]
     public string? Name { get; set; }
+    [Required]
     public string? Description { get; set; }
+    [DataType(DataType.Currency)]
+    [Range(0.01, 1000.0, ErrorMessage = "Value for {0} must be between" +
+                                        "{1:C} and {2:C}")]
     public decimal Price { get; set; }
     public bool IsActive { get; set; }
 
